9dfa3dd5927be188695f125268416d509ad213fd
Jose Carlos <josecarlosmartinez@gmail.com>
2019-03-16 09:14:35 +0100

    first commit

---
+++ src/main/java/org/jocamav/windfarm/WindFarmManagerApplication.java
@@ -0,0 +1,13 @@
+package org.jocamav.windfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindFarmManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindFarmManagerApplication.class, args);
+	}
+
+}
